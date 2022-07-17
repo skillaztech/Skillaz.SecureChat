2a532bf35c7e3363457e4ae075bd398bed6e8fe9
picolino <webmaster.pico@gmail.com>
2022-07-17 12:45:44 +0300

    Add udp broadcasting

--- chat/Chat.fs
+++ chat/Chat.fs
@@ -1,8 +1,8 @@
 ﻿namespace chat
 
 open System
-open System.IO
 open System.Net
+open System.Net.Sockets
 open System.Text
 open System.Text.Json
 open Elmish
@@ -19,15 +19,35 @@ module Chat =
         CurrentUser: string
         MessageInput: string
         MessagesList: Message list
+        TcpEndpoint: IPEndPoint option
     }
         
     type Msg =
-        | P2pMessageReceived of Message
+        | UdpSendPackage of UdpClient * int
+        | UdpPackageReceived of byte[] * IPEndPoint
+        | MessageReceived of Message
         | TextChanged of string
         | SendMessage
+        
+    let udpSubscription listener =
+        fun dispatch ->
+            let invoke (payload:byte[]) endpoint =
+                Msg.UdpPackageReceived (payload, endpoint) |> dispatch |> ignore
+            P2PNetwork.listenForUdpPackage listener invoke |> Async.Start
+        
+    let tcpSubscription (ip:IPEndPoint) =
+        fun dispatch ->
+            let invoke buf read =
+                let json = Encoding.UTF8.GetString(buf, 0, read)
+                let msg = JsonSerializer.Deserialize<Message>(json)
+                Msg.MessageReceived msg |> dispatch |> ignore
+            
+            let listener = P2PNetwork.tcpListener ip.Address ip.Port
+            P2PNetwork.listenForTcpPackage listener invoke |> Async.Start
     
     let init =
         let model = {
+            TcpEndpoint = None
             CurrentUser = "Me"
             MessageInput = ""
             MessagesList = [
@@ -42,24 +62,39 @@ module Chat =
             ]
         }
         
-        model, Cmd.none
+        let port = 63211
+        let udpClient = P2PNetwork.udpClient IPAddress.Any port
+        
+        let cmd = Cmd.batch [
+            Cmd.ofSub <| udpSubscription udpClient
+            Cmd.ofMsg <| UdpSendPackage (udpClient, port)
+        ]
+        
+        model, cmd
     
     let update msg model =
         match msg with
-        | P2pMessageReceived m ->
+        | UdpSendPackage (client, port) ->
+            let payload = Encoding.UTF8.GetBytes("") // TODO: Inject secret
+            client.Send(payload, payload.Length, IPEndPoint(IPAddress.Broadcast, port)) |> ignore
+            model, Cmd.none
+        | UdpPackageReceived (payload, ip) ->
+            let payload = Encoding.UTF8.GetString(payload)
+            if payload = "" // TODO: Parse secret
+            then { model with TcpEndpoint = Some ip }, Cmd.ofSub <| tcpSubscription ip
+            else model, Cmd.none
+        | MessageReceived m ->
             { model with MessagesList = model.MessagesList @ [m] }, Cmd.none
         | SendMessage ->
-            use c = P2PNetwork.client IPAddress.Loopback 5001
-            use stream = c.GetStream()
             let newMsg = {
                 Sender = model.CurrentUser
                 DateTime = DateTime.Now
                 Message = model.MessageInput
             }
-            let message = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(newMsg))
-            stream.Write(message)
-            stream.Flush()
-            { model with MessageInput = "" }, Cmd.none
+            let ip = model.TcpEndpoint.Value
+            use client = P2PNetwork.tcpClient ip.Address ip.Port
+            P2PNetwork.tcpSendAsJson client newMsg
+            { model with MessageInput = ""; MessagesList = model.MessagesList @ [newMsg] }, Cmd.none
         | TextChanged t ->
             { model with MessageInput = t }, Cmd.none
 
@@ -150,6 +185,7 @@ module Chat =
                     Button.horizontalAlignment HorizontalAlignment.Center
                     Button.horizontalContentAlignment HorizontalAlignment.Center
                     Button.content ">"
+                    Button.isEnabled model.TcpEndpoint.IsSome
                     Button.onClick (fun _ -> dispatch SendMessage)
                 ]
             ]
--- chat/P2PNetwork.fs
+++ chat/P2PNetwork.fs
@@ -2,21 +2,38 @@ namespace chat
 
 open System.Net
 open System.Net.Sockets
+open System.Text
+open System.Text.Json
 
 module P2PNetwork =
-    let listener ip port =
+    let tcpListener ip port =
         let tcp = TcpListener(ip, port)
         tcp.Server.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true)
         tcp
-    let client (ip:IPAddress) port =
+    let tcpClient (ip:IPAddress) port =
         new TcpClient(IPEndPoint(ip, port))
         
-    let rec listenForPackage (tcp:TcpListener) dispatch = async {
+    let rec listenForTcpPackage (tcp:TcpListener) dispatch = async {
         tcp.Start()
         let! tcpClient = tcp.AcceptTcpClientAsync() |> Async.AwaitTask
         let networkStream = tcpClient.GetStream()
         let buffer = Array.zeroCreate tcpClient.ReceiveBufferSize
         let read = networkStream.Read(buffer, 0, tcpClient.ReceiveBufferSize)
         dispatch buffer read
-        do! listenForPackage tcp dispatch
+        do! listenForTcpPackage tcp dispatch
+    }
+    
+    let tcpSendAsJson (tcp:TcpClient) payload =
+        use stream = tcp.GetStream()
+        let message = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(payload))
+        stream.Write(message)
+        stream.Flush()
+    
+    let udpClient (ip:IPAddress) port =
+        new UdpClient(IPEndPoint(ip, port))
+    
+    let rec listenForUdpPackage (udp:UdpClient) dispatch = async {
+        let! receiveResult = udp.ReceiveAsync() |> Async.AwaitTask
+        dispatch receiveResult.Buffer receiveResult.RemoteEndPoint
+        do! listenForUdpPackage udp dispatch
     }
