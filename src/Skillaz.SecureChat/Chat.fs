a3b5cf8deb323e3b2d1575ee67360a7f8f6d70f2
picolino <webmaster.pico@gmail.com>
2022-12-01 20:58:58 +0300

    Add secret code editing

--- src/Skillaz.SecureChat/Chat.fs
+++ src/Skillaz.SecureChat/Chat.fs
@@ -43,6 +43,7 @@ module Chat =
     
     type Model = {
         AppSettings: AppSettingsJson.Root
+        SecretCode: int
         CurrentUserName: string
         CurrentAppMark: string
         TcpListener: Socket
@@ -51,7 +52,6 @@ module Chat =
         UnixSocketListener: Socket
         MessageInput: string
         MessagesList: LocalMessage list
-        SecretCodeVisible: bool
         Connections: ConnectedEndpoint list
         ConnectedApps: ConnectedApp list
         SettingsVisible: bool
@@ -77,8 +77,8 @@ module Chat =
         | TextChanged of string
         | AppendLocalMessage of LocalMessage
         | SendMessage
-        | ToggleSecretCodeVisibility
         | ToggleSettingsVisibility
+        | SecretCodeChanged of int
         
     let connectionsSubscription listener dispatch =
         let handle socket =
@@ -134,6 +134,7 @@ module Chat =
         
         let model = {
             AppSettings = appSettings
+            SecretCode = appSettings.SecretCode
             CurrentUserName = Environment.UserName
             CurrentAppMark = appMark
             TcpListener = Tcp.listener
@@ -144,7 +145,6 @@ module Chat =
             ConnectedApps = []
             MessageInput = ""
             MessagesList = []
-            SecretCodeVisible = false
             SettingsVisible = false;
         }
         
@@ -280,7 +280,7 @@ module Chat =
                             let msg = {
                                 MessageSender = model.CurrentUserName
                                 AppMark = model.CurrentAppMark
-                                SecretCode = model.AppSettings.SecretCode
+                                SecretCode = model.SecretCode
                                 RetranslationInfo = {
                                     RetranslatedBy = [ model.CurrentAppMark ]
                                 }
@@ -303,7 +303,7 @@ module Chat =
             match msg.RetranslationInfo.RetranslatedBy |> List.contains model.CurrentAppMark with
             | false ->
                 let apps =
-                    match model.AppSettings.SecretCode = msg.SecretCode && msg.AppMark <> model.CurrentAppMark with
+                    match model.SecretCode = msg.SecretCode && msg.AppMark <> model.CurrentAppMark with
                     | true ->
                         model.ConnectedApps
                         |> List.upsert
@@ -332,7 +332,7 @@ module Chat =
                     DateTime = DateTime.Now
                     MessageText = model.MessageInput
                     MessageSender = model.CurrentUserName
-                    SecretCode = model.AppSettings.SecretCode
+                    SecretCode = model.SecretCode
                     AppMark = model.CurrentAppMark
                     RetranslationInfo = {
                         RetranslatedBy = [ model.CurrentAppMark ]
@@ -352,7 +352,7 @@ module Chat =
         | RemoteChatMessageReceived (msg, client) ->
             match msg.RetranslationInfo.RetranslatedBy |> List.contains model.CurrentAppMark with
             | false ->
-                match msg.SecretCode = model.AppSettings.SecretCode with
+                match msg.SecretCode = model.SecretCode with
                 | true ->
                     let cmds = [
                         Cmd.ofMsg <| AppendLocalMessage { Message = msg; IsMe = false }
@@ -388,10 +388,10 @@ module Chat =
             { model with MessagesList = m :: model.MessagesList }, Cmd.none
         | TextChanged t ->
             { model with MessageInput = t }, Cmd.none
-        | ToggleSecretCodeVisibility ->
-            { model with SecretCodeVisible = not model.SecretCodeVisible }, Cmd.none
         | ToggleSettingsVisibility ->
             { model with SettingsVisible = not model.SettingsVisible }, Cmd.none
+        |  SecretCodeChanged secretCode ->
+            { model with SecretCode = secretCode }, Cmd.none
 
     let view model dispatch =
         Grid.create [
@@ -462,7 +462,7 @@ module Chat =
                     Grid.column 1
                     Grid.row 3
                     Grid.background "#fafafa"
-                    Grid.columnDefinitions "5, *, 5, Auto, 5"
+                    Grid.columnDefinitions "5, Auto, 5, *, 5"
                     Grid.rowDefinitions "5, Auto, 5"
                     Grid.isVisible model.SettingsVisible
                     Grid.children [
@@ -470,23 +470,23 @@ module Chat =
                             TextBlock.column 1
                             TextBlock.row 1
                             TextBlock.classes [ "label-secret-code" ]
-                            match model.SecretCodeVisible with
-                            | true -> TextBlock.text $"Код: {model.AppSettings.SecretCode}"
-                            | false -> TextBlock.text "Код: ******"
+                            TextBlock.text $"Код:"
                         ]
-                        Button.create [
-                            Button.column 3
-                            TextBlock.row 1
-                            Button.classes [ "button-show-secret-code" ]
-                            Button.content (
-                                Path.create [
-                                    Shapes.Path.classes [ "button-show-secret-code-icon" ]
-                                    match model.SecretCodeVisible with
-                                    | true -> Shapes.Path.data "M11.83,9L15,12.16C15,12.11 15,12.05 15,12A3,3 0 0,0 12,9C11.94,9 11.89,9 11.83,9M7.53,9.8L9.08,11.35C9.03,11.56 9,11.77 9,12A3,3 0 0,0 12,15C12.22,15 12.44,14.97 12.65,14.92L14.2,16.47C13.53,16.8 12.79,17 12,17A5,5 0 0,1 7,12C7,11.21 7.2,10.47 7.53,9.8M2,4.27L4.28,6.55L4.73,7C3.08,8.3 1.78,10 1,12C2.73,16.39 7,19.5 12,19.5C13.55,19.5 15.03,19.2 16.38,18.66L16.81,19.08L19.73,22L21,20.73L3.27,3M12,7A5,5 0 0,1 17,12C17,12.64 16.87,13.26 16.64,13.82L19.57,16.75C21.07,15.5 22.27,13.86 23,12C21.27,7.61 17,4.5 12,4.5C10.6,4.5 9.26,4.75 8,5.2L10.17,7.35C10.74,7.13 11.35,7 12,7Z"
-                                    | false -> Shapes.Path.data "M12,9A3,3 0 0,0 9,12A3,3 0 0,0 12,15A3,3 0 0,0 15,12A3,3 0 0,0 12,9M12,17A5,5 0 0,1 7,12A5,5 0 0,1 12,7A5,5 0 0,1 17,12A5,5 0 0,1 12,17M12,4.5C7,4.5 2.73,7.61 1,12C2.73,16.39 7,19.5 12,19.5C17,19.5 21.27,16.39 23,12C21.27,7.61 17,4.5 12,4.5Z"
-                                ]
+                        NumericUpDown.create [
+                            NumericUpDown.column 3
+                            NumericUpDown.row 1
+                            NumericUpDown.classes [ "label-secret-code" ]
+                            NumericUpDown.allowSpin false
+                            NumericUpDown.showButtonSpinner false
+                            NumericUpDown.minimum 100000
+                            NumericUpDown.maximum 999999
+                            NumericUpDown.value model.SecretCode
+                            NumericUpDown.onTextChanged (
+                                fun o ->
+                                    let parseSuccess, parsedValue = Int32.TryParse(o)
+                                    if parseSuccess
+                                    then parsedValue |> SecretCodeChanged |> dispatch
                             )
-                            Button.onClick (fun _ -> ToggleSecretCodeVisibility |> dispatch)
                         ]
                     ]
                 ]
