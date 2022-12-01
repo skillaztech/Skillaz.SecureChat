4a636223f99d7ffccae36af444011706061e587f
picolino <webmaster.pico@gmail.com>
2022-12-01 16:12:47 +0300

    Fix access control for unix sockets directory

--- src/Skillaz.SecureChat/p2p/UnixSocket.fs
+++ src/Skillaz.SecureChat/p2p/UnixSocket.fs
@@ -2,12 +2,19 @@
 
 open System.IO
 open System.Net.Sockets
+open System.Security.AccessControl
+open System.Security.Principal
 open Skillaz.SecureChat.P2PNetwork
 
 module UnixSocket =
     
     let listener (path:string) =
-        Directory.CreateDirectory(Path.GetDirectoryName(path)) |> ignore
+        let directory = Directory.CreateDirectory(Path.GetDirectoryName(path))
+        
+        let accessControl = directory.GetAccessControl()
+        accessControl.AddAccessRule(FileSystemAccessRule(new SecurityIdentifier(WellKnownSidType.WorldSid, null), FileSystemRights.FullControl, InheritanceFlags.ObjectInherit ||| InheritanceFlags.ContainerInherit, PropagationFlags.NoPropagateInherit, AccessControlType.Allow));
+        directory.SetAccessControl(accessControl)
+        
         File.Delete(path)
         let socket = new Socket(AddressFamily.Unix, SocketType.Stream, ProtocolType.IP)
         socket.Bind(UnixDomainSocketEndPoint(path))
