1bef8609291197e7ab7fc89ddeeeb1566d236eb9
d.chervonny <d.chervonny@skillaz.ru>
2023-03-15 18:28:30 +0300

    Add application lifetime mock

--- tests/TestHelpers.fs
+++ tests/TestHelpers.fs
@@ -1,12 +1,21 @@
 ﻿module Skillaz.SecureChat.AcceptanceTests.TestHelpers
 
 open System.Net.Sockets
+open Avalonia.Controls.ApplicationLifetimes
 open Skillaz.SecureChat.Chat
 open Skillaz.SecureChat.ChatArgs
 open Skillaz.SecureChat.IO.IOsDetector
 
 let emptyArgs =
     {
+        ApplicationLifetime = {
+            new IControlledApplicationLifetime with
+            member _.add_Startup(value) = ()
+            member _.remove_Startup(value) = ()
+            member _.add_Exit(value) = ()
+            member _.remove_Exit(value) = ()
+            member _.Shutdown(exitCode) = ()
+        }
         Version = "v1.0.0"
         ProcessDirectory = ""
         OsDetector = {
