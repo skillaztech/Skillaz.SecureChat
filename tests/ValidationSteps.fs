5b66b96221b7632bace168967f8996617644d9e5
picolino <webmaster.pico@gmail.com>
2023-07-14 22:57:06 +0700

    Add username change in validation tests

--- tests/ValidationSteps.fs
+++ tests/ValidationSteps.fs
@@ -7,6 +7,7 @@ open Skillaz.SecureChat.Chat
 open TickSpec
 
 type ValidationSteps () =
+    let mutable newUserName = ""
     
     [<Given>]
     member _.``started instance`` () =
@@ -16,9 +17,18 @@ type ValidationSteps () =
     member this.``user set username between (.*) and (.*) chars length`` (minSize: int) (maxSize: int) (model: Model) =
         let size = Random.Shared.Next(minSize, maxSize + 1)
         let userName = String(Array.init size (fun _ -> 'o'))
+        newUserName <- userName
         let msg = UserNameChanged userName
         Chat.update msg model |> fst
         
     [<Then>]
     member _.``validation errors count should be (.*)`` (errorsCount: int) (model: Model) =
         Expect.hasLength model.UserNameValidationErrors errorsCount ""
+        
+    [<Then>]
+    member _.``username should not be changed`` (model: Model) =
+        Expect.notEqual model.UserName newUserName ""
+        
+    [<Then>]
+    member _.``username should be changed`` (model: Model) =
+        Expect.equal model.UserName newUserName ""
