5bb5341a7098eac7ed2bb2f0de590ee370835268
picolino <webmaster.pico@gmail.com>
2023-07-18 01:42:10 +0700

    Separate two tests unix sockets storages

--- tests/ArgsBuilder.fs
+++ tests/ArgsBuilder.fs
@@ -5,7 +5,6 @@ open Avalonia.Controls.ApplicationLifetimes
 open Skillaz.SecureChat.AcceptanceTests.TestConfigStorage
 open Skillaz.SecureChat.ChatArgs
 open Skillaz.SecureChat.Domain.Domain
-open Skillaz.SecureChat.IConfigStorage
 open Skillaz.SecureChat.INetworkProvider
 open Skillaz.SecureChat.P2P
 
@@ -35,8 +34,8 @@ let mkArgs =
         LogLevel = "Fatal"
     }
     UserSettings = userSettings
-    UnixSocketsFolderPath = "./tests"
-    UnixSocketFilePath = "test.socket"
+    UnixSocketsFolderPath = "./tests-sockets/local"
+    UnixSocketFilePath = "local.socket"
     NetworkProvider = {
                 new INetworkProvider with
                     member this.RemoteListener = {
@@ -44,17 +43,17 @@ let mkArgs =
                             member this.Socket = DefaultSockets.remoteListener
                             member this.IsBound = DefaultSockets.remoteListener.IsBound
                             member this.StartListen() = DefaultSockets.remoteListener.Listen()
-                            member this.Bind() = UnixSocket.tryBindTo "./tests/remote.socket" DefaultSockets.remoteListener
+                            member this.Bind() = UnixSocket.tryBindTo "./tests-sockets/remote/remote.socket" DefaultSockets.remoteListener
                             member this.GenerateClient _ = UnixSocket.client
-                            member this.Connect _ socket = UnixSocket.connectSocket socket "./tests/remote.socket"}
+                            member this.Connect _ socket = UnixSocket.connectSocket socket "./tests-sockets/remote/remote.socket"}
                     member this.LocalListener = {
                         new INetworkListener with
                             member this.Socket = DefaultSockets.localListener
                             member this.IsBound = DefaultSockets.localListener.IsBound
                             member this.StartListen() = DefaultSockets.localListener.Listen()
-                            member this.Bind() = UnixSocket.tryBindTo "./tests/local.socket" DefaultSockets.localListener
+                            member this.Bind() = UnixSocket.tryBindTo "./tests-sockets/local/local.socket" DefaultSockets.localListener
                             member this.GenerateClient _ = UnixSocket.client
-                            member this.Connect _ socket = UnixSocket.connectSocket socket "./tests/local.socket"
+                            member this.Connect _ socket = UnixSocket.connectSocket socket "./tests-sockets/local/local.socket"
                     }
     } 
 }
