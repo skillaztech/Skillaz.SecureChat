d9fbcc9017f984877e290b68904b5b0afe9dcb95
picolino <webmaster.pico@gmail.com>
2023-07-18 20:15:39 +0700

    Create directories for tests sockets

--- tests/ArgsBuilder.fs
+++ tests/ArgsBuilder.fs
@@ -1,6 +1,7 @@
 module Skillaz.SecureChat.AcceptanceTests.ArgsBuilder
 
 open System
+open System.IO
 open Avalonia.Controls.ApplicationLifetimes
 open Skillaz.SecureChat.AcceptanceTests.TestConfigStorage
 open Skillaz.SecureChat.ChatArgs
@@ -14,6 +15,11 @@ let mkArgs =
         Name = Guid.NewGuid().ToString()
         SecretCode = Random.Shared.Next(100000, 999999)
     }
+    
+    let localSocketsFolder = "./tests-sockets/local"
+    let remoteSocketsFolder = "./tests-sockets/remote"
+    Directory.CreateDirectory(localSocketsFolder) |> ignore
+    Directory.CreateDirectory(remoteSocketsFolder) |> ignore
     
     {
     ApplicationLifetime = {
@@ -34,7 +40,7 @@ let mkArgs =
         LogLevel = "Fatal"
     }
     UserSettings = userSettings
-    UnixSocketsFolderPath = "./tests-sockets/local"
+    UnixSocketsFolderPath = localSocketsFolder
     UnixSocketFilePath = "local.socket"
     NetworkProvider = {
                 new INetworkProvider with
@@ -43,17 +49,17 @@ let mkArgs =
                             member this.Socket = DefaultSockets.remoteListener
                             member this.IsBound = DefaultSockets.remoteListener.IsBound
                             member this.StartListen() = DefaultSockets.remoteListener.Listen()
-                            member this.Bind() = UnixSocket.tryBindTo "./tests-sockets/remote/remote.socket" DefaultSockets.remoteListener
+                            member this.Bind() = UnixSocket.tryBindTo $"{remoteSocketsFolder}/remote.socket" DefaultSockets.remoteListener
                             member this.GenerateClient _ = UnixSocket.client
-                            member this.Connect _ socket = UnixSocket.connectSocket socket "./tests-sockets/remote/remote.socket"}
+                            member this.Connect _ socket = UnixSocket.connectSocket socket $"{remoteSocketsFolder}/remote/remote.socket"}
                     member this.LocalListener = {
                         new INetworkListener with
                             member this.Socket = DefaultSockets.localListener
                             member this.IsBound = DefaultSockets.localListener.IsBound
                             member this.StartListen() = DefaultSockets.localListener.Listen()
-                            member this.Bind() = UnixSocket.tryBindTo "./tests-sockets/local/local.socket" DefaultSockets.localListener
+                            member this.Bind() = UnixSocket.tryBindTo $"{localSocketsFolder}/local.socket" DefaultSockets.localListener
                             member this.GenerateClient _ = UnixSocket.client
-                            member this.Connect _ socket = UnixSocket.connectSocket socket "./tests-sockets/local/local.socket"
+                            member this.Connect _ socket = UnixSocket.connectSocket socket $"{localSocketsFolder}/local.socket"
                     }
     } 
 }
