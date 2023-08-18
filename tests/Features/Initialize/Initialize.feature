488f543e7b78c0a56ee1f24c0f37a72191d8b7f7
picolino <webmaster.pico@gmail.com>
2023-08-18 11:29:47 +0300

    Add another initialize tests

--- tests/Features/Initialize/Initialize.feature
+++ tests/Features/Initialize/Initialize.feature
@@ -1,8 +1,15 @@
 ﻿Feature: Initialize
   As application user
-  I want to application starts without errors
+  I want to application starts properly and without errors
   
   Scenario: Application starts without errors
     Given default application configuration
     When application starts
     Then no errors occurs
+    
+  Scenario: If application port is not busy then remote listener and local listener both should bound on sockets
+    Given default application configuration
+    When application starts
+    And initial commands executes
+    Then remote listener is bounded
+    And local listener is bounded
--- tests/Features/Initialize/InitializeSteps.fs
+++ tests/Features/Initialize/InitializeSteps.fs
@@ -1,6 +1,7 @@
 ﻿module Skillaz.SecureChat.AcceptanceTests.InitializeSteps
 
 open System
+open Elmish
 open Skillaz.SecureChat
 open Skillaz.SecureChat.ChatArgs
 open TickSpec
@@ -11,11 +12,24 @@ type InitializeSteps () =
     [<When>]
     member _.``application starts`` (args: ChatArgs) =
         try
-            Chat.init args |> ignore
-            None
+            Chat.init args |> Result.Ok
         with
-        | e -> Some e
+        | e -> Result.Error e
+        
+    [<When>]
+    member _.``initial commands executes`` (initResult: Result<Chat.Model * Cmd<Chat.Msg>, Exception>) =
+        match initResult with
+        | Ok (model, cmd) -> Cmd.execm (model, cmd)
+        | Error e -> failtest <| e.ToString()
         
     [<Then>]
-    member _.``no errors occurs`` (exn: Exception option) =
-        Expect.isNone exn "Exception throws when app starts"
+    member _.``remote listener is bounded`` () =
+        Expect.isTrue DefaultSockets.remoteListener.IsBound
+        
+    [<Then>]
+    member _.``local listener is bounded`` () =
+        Expect.isTrue DefaultSockets.localListener.IsBound
+        
+    [<Then>]
+    member _.``no errors occurs`` (initResult: Result<(Chat.Model * Cmd<Chat.Msg>), Exception>) =
+        Expect.isOk initResult "Exception throws when app starts"
